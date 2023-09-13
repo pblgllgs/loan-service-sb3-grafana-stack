0934912af48fc1e7b260ea544267b8393560e95d
pbl.gllgs <pbl.gllgs@gmail.com>
2023-09-13 01:08:50 -0300

    Delete target/classes directory

--- target/classes/data.sql
+++
@@ -1,6 +0,0 @@
-truncate table loans;
-ALTER TABLE loans AUTO_INCREMENT = 1;
-INSERT INTO loans (id, loanId, customerName, customerId, amount, loanStatus)
-VALUES (1, UUID(), 'John', 101, 5000.00, 'APPROVED'),
-       (2, UUID(), 'Sai', 102, 7500.00, 'APPROVED'),
-       (3, UUID(), 'Alice', 103, 3000.00, 'REJECTED');
